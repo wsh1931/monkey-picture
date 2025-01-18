06928d3044013dca63ac48473b97dc0280353222
wsh <1931443283@qq.com>
2025-01-18 08:09:59 +0800

    完成后端项目初始化

---
+++ monkey-picture-backend/src/main/java/com/wusihao/monkeypicturebackend/MonkeyPictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wusihao.monkeypicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkeyPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonkeyPictureBackendApplication.class, args);
+    }
+
+}
---
+++ monkey-picture-backend/src/test/java/com/wusihao/monkeypicturebackend/MonkeyPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wusihao.monkeypicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonkeyPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
