7aa658138ac4e91687c2b48f2bccd58e4073400e
wsh <1931443283@qq.com>
2025-01-22 22:35:37 +0800

    引入常用依赖，工具类

--- monkey-picture-backend/src/main/java/com/wusihao/monkeypicturebackend/MonkeyPictureBackendApplication.java
+++ monkey-picture-backend/src/main/java/com/wusihao/monkeypicturebackend/MonkeyPictureBackendApplication.java
@@ -1,9 +1,13 @@
 package com.wusihao.monkeypicturebackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@MapperScan("com/wusihao/monkeypicturebackend/mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class MonkeyPictureBackendApplication {
 
     public static void main(String[] args) {
